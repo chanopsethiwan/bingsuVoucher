bc9f74a6233103ebda378a08b502bb4cb086e57c
paopaoch <paopaochofficial@gmail.com>
2021-07-29 18:16:48 +0700

    fix get available vouchers

--- bingsuVoucher/src/app.py
+++ bingsuVoucher/src/app.py
@@ -60,6 +60,17 @@ def get_available_vouchers(event, context):
 def get_voucher_by_type(event, context):
     from pandas import DataFrame
     item = event['arguments']
+    dynamodb = boto3.resource('dynamodb')
+    
+    # get user info.
+    user_id = item['user_id']
+    user_table = dynamodb.Table('BingsuUser')
+    response_user = user_table.query(
+        KeyConditionExpression=Key('user_id').eq(user_id)
+    )
+    old_coins = response_user['Items'][0]['coins']
+
+    # get voucher
     voucher_type = item['voucher_type']
     dynamodb = boto3.resource('dynamodb')
     voucher_table = dynamodb.Table('BingsuVoucher')
@@ -70,19 +81,42 @@ def get_voucher_by_type(event, context):
     df = df[df['status'] == 'Available']
     if len(df) == 0:
         return {'status': 400, 'voucher_id': "No available voucher"}
-    voucher_id = str(df['voucher_id'].iloc[0])
-    voucher_item = PynamoBingsuVoucher(
-        voucher_id = str(df['voucher_id'].iloc[0]),
-        date_time = str(df['date_time'].iloc[0]),
-        description = str(df['description'].iloc[0]),
-        status = 'Unavailable',
-        title = str(df['title'].iloc[0]),
-        voucher_type = voucher_type,
-        icon_name = str(df['icon_name'].iloc[0]),
-        voucher_conditions = str(df['voucher_conditions'].iloc[0]),
-        voucher_detail = str(df['voucher_detail'].iloc[0]),
-        coin_needed = int(df['coin_needed'].iloc[0])
-    )
-    voucher_item.save()
+    voucher_price = int(df['coin_needed'].iloc[0])
+    # check transaction
+    if old_coins < voucher_price:
+        return {'status': 230, 'voucher_id': "User does not have enough coins"}
+    else:
+        # deduct coins from user
+        new_coins = old_coins - voucher_price
+        client_lambda = boto3.client('lambda')
+        arguments = {
+            "user_id": user_id,
+            "coins": new_coins,
+        }
 
-    return {'status': 200, 'voucher_id': voucher_id}
+        update_user_response = client_lambda.invoke(
+            FunctionName = 'arn:aws:lambda:ap-southeast-1:405742985670:function:bingsuUser-UpdateUserFunction-9I54tc4Xyb2h',
+            InvocationType = 'RequestResponse',
+            Payload = json.dumps({'arguments': arguments})
+        )
+        update_user_status =  str(json.load(update_user_response['Payload'])['status'])
+        if update_user_status == 400:
+            return {'status': 400, 'voucher_id': "Failed to update user table no coins have been deducted"}
+
+        # set voucher as Unavailable
+        voucher_id = str(df['voucher_id'].iloc[0])
+        voucher_item = PynamoBingsuVoucher(
+            voucher_id = str(df['voucher_id'].iloc[0]),
+            date_time = str(df['date_time'].iloc[0]),
+            description = str(df['description'].iloc[0]),
+            status = 'Unavailable',
+            title = str(df['title'].iloc[0]),
+            voucher_type = voucher_type,
+            icon_name = str(df['icon_name'].iloc[0]),
+            voucher_conditions = str(df['voucher_conditions'].iloc[0]),
+            voucher_detail = str(df['voucher_detail'].iloc[0]),
+            coin_needed = int(df['coin_needed'].iloc[0])
+        )
+        voucher_item.save()
+
+        return {'status': 200, 'voucher_id': voucher_id}
